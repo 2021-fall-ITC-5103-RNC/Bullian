8f8d50223b55a9af3650f70d97192df3fd40e561
PriyaMJoseph <n01468981@humbermail.ca>
2021-12-07 13:38:35 -0500

    faq script file

--- faq/scripts/faq.js
+++ faq/scripts/faq.js
@@ -7,3 +7,29 @@ for (var i = 0; i < faq.length; i++) {
         this.nextElementSibling.classList.toggle("show");
     });
 }
+
+const searchForm = document.getElementById('form');
+const searchInput = document.getElementById('searchbar');
+let questions = document.getElementsByClassName('faq');
+searchForm.addEventListener('
+', (event) => {
+const { value } = searchInput;
+
+// get user search input converted to lowercase
+const searchQuery = value.toLowerCase();
+
+for (const element of questions) {
+    let qn = element.textContent.toLowerCase();
+
+    if (qn.includes(searchQuery)) {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
+});
+
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener("click", () => {
+    document.getElementById('searchbar').value = "";
+});
